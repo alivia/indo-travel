5c209e6ce5415d8d23dea6243ee67f8635621c58
Alivia Savira <Alivia@Alivias-MacBook-Air.local>
2017-10-22 17:13:26 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('header', { duration: 900 });
+sr.reveal('.header-desc', {origin: 'right', distance: '600px', duration: 1000});
+sr.reveal('.title', {origin: 'left', distance: '600px', duration: 1000});
+sr.reveal('myButton', { duration: 900 });
+sr.reveal('.autowide .col img', { duration: 1500 });
