5444845fa60435e4a9915943a256e4da360d1616
Alivia Savira <aliviaayusavira@gmail.com>
2017-11-16 10:08:36 +0900

    footer

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ sr.reveal('.header-desc', {origin: 'right', distance: '600px', duration: 1000});
 sr.reveal('.header-content .title', {origin: 'left', distance: '600px', duration: 1000});
 sr.reveal('myButton', { duration: 900 });
 sr.reveal('.autowide .col img', { duration: 1500 });
+sr.reveal('#map', {origin: 'right', distance: '600px', duration: 1000});
 sr.reveal('#plan section .mainpic', { duration: 1500 });
